5d931ec1317facc202019c48dc0e73636d96a258
Asif Akhtar <146723391+asifprofessionalboy@users.noreply.github.com>
2025-01-08 18:30:27 +0530

    Add files via upload

---
+++ EmailService.cs
@@ -0,0 +1,47 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using MimeKit.Text;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using Humanizer.Configuration;
+namespace GFAS.Email
+{
+    public class EmailService
+    {
+        private readonly IConfiguration configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string ccEmail, string bccEmail, string subject, string message)
+        {
+            var emailSettings = configuration.GetSection("EmailSettings");
+            var email = new MimeMessage();
+            email.From.Add(new MailboxAddress(emailSettings["SenderName"], emailSettings["SenderEmail"]));
+            email.To.Add(new MailboxAddress(toEmail, toEmail));
+
+            if (!string.IsNullOrEmpty(ccEmail))
+            {
+                email.Cc.Add(new MailboxAddress(ccEmail, ccEmail));
+            }
+            if (!string.IsNullOrEmpty(bccEmail))
+            {
+                email.Bcc.Add(new MailboxAddress(bccEmail, bccEmail));
+            }
+            email.Subject = subject;
+            email.Body = new TextPart(TextFormat.Html)
+            {
+                Text = message
+            };
+
+            using (var smtp = new SmtpClient())
+            {
+                smtp.Connect(emailSettings["SMTPServer"], int.Parse(emailSettings["SMTPPort"]), MailKit.Security.SecureSocketOptions.None);
+                await smtp.SendAsync(email);
+                smtp.Disconnect(true);
+            }
+        }
+    }
+}
---
+++ Hash_Password.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Security.Cryptography;
+namespace GFAS.PasswordHasher
+{
+    public class Hash_Password
+    {
+        public (string hashedPassword, string salt) HashPassword(string password)
+        {
+
+            byte[] saltBytes = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(saltBytes);
+            }
+
+
+            string salt = Convert.ToBase64String(saltBytes);
+
+
+            string hashedPassword = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: saltBytes,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+
+            return (hashedPassword, salt);
+        }
+
+        public bool VerifyPassword(string enteredPassword, string storedHash, string storedSalt)
+        {
+
+            byte[] saltBytes = Convert.FromBase64String(storedSalt);
+
+
+            string hashedPassword = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: enteredPassword,
+                salt: saltBytes,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+
+
+            return hashedPassword == storedHash;
+        }
+    }
+}
